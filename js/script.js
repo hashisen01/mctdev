da54ce85b1511ed1ecd3702572f025a9bd5171fc
hashisen01 <matheuscostafer159@gmail.com>
2021-12-28 10:01:46 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+var digitando = new Typed(".digitando",{
+    strings:["","Desenvolvedor de Sistemas.","Desenvolvedor Front-End.","Desenvolvedor Mobile."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
